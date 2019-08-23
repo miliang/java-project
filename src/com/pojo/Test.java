c0acf9991f800699c4f434bc55d42eb6ab1c4c68
miliang <1240894958@qq.com>
2019-08-23 18:25:02 +0800

    添加

--- src/com/pojo/Test.java
+++ src/com/pojo/Test.java
@@ -1,10 +1,13 @@
 package com.pojo;
 
+import com.beans.User;
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
-
+		User use = new User("ÕÅÈı",22,"ÄĞ");
+		System.out.println(use);
 	}
 
 }
